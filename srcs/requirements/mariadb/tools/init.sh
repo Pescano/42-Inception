38718cc27689fd82abc098b5f242383d3863577d
paescano <88772018+Pescano@users.noreply.github.com>
2024-03-08 19:33:01 +0100

    MOD: MariaDB fixed init.sh querys

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-service mysql start 
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIA_DB_NAME}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MARIA_DB_USER}\`@'localhost' IDENTIFIED BY '${MARIA_DB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIA_DB_NAME}\`.* TO \`${MARIA_DB_USER}\`@'%' IDENTIFIED BY '${MARIA_DB_PASS}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_DB_ROOT_PASS}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$MARIA_DB_ROOT_PASS shutdown
+exec mysqld_safe
 
-echo "CREATE DATABASE IF NOT EXISTS \`${MARIA_DB_NAME}\`;" > msql_db.sql
-echo "CREATE USER IF NOT EXISTS \`${MARIA_DB_USER}\`@'localhost' IDENTIFIED BY '${MARIA_DB_PASS}';" >> msql_db.sql
-echo "GRANT ALL PRIVILEGES ON \`${MARIA_DB_NAME}\`.* TO \`${MARIA_DB_USER}\`@'%' IDENTIFIED BY '${MARIA_DB_PASS}';" >> msql_db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_DB_ROOT_PASS}';" >> msql_db.sql
-echo "FLUSH PRIVILEGES;" >> msql_db.sql
-
-mysql < msql_db.sql
