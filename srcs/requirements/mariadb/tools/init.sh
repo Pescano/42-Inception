a8a1101d14f01811bb50e80fd0e531291e08b1d7
paescano <pablo.escano.martin@gmail.com>
2024-03-11 03:27:06 +0100

    ADD: WordPress MOD: NGINX conf & MariaDB script v0.4.0

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIA_DB_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MARIA_DB_USER}\`@'localhost' IDENTIFIED BY '${MARIA_DB_PASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MARIA_DB_NAME}\`.* TO \`${MARIA_DB_USER}\`@'%' IDENTIFIED BY '${MARIA_DB_PASS}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_DB_ROOT_PASS}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$MARIA_DB_ROOT_PASS shutdown
-exec mysqld_safe
+service mariadb start
 
+mysql -e "create database if not exists $MARIA_DB_NAME;"
+mysql -e "create user if not exists '$MARIA_DB_USER'@'%' identified by '$MARIA_DB_PASS';"
+mysql -e "grant all privileges on *.* TO '$MARIA_DB_USER'@'%' with grant option;"
+mysql -e "flush privileges;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIA_DB_ROOT_PASS';"
+pkill -f mysqld
+
+mysqld
